47a70061d5966123069394a9cbfbdc724942bade
Nguyen Tien Si <simavel4.0@gmail.com>
2024-03-10 19:05:57 +0700

    first commit

---
+++ ReadNews.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, Image } from 'react-native'
+
+function ReadNews() {
+    return (
+        <View style={{ backgroundColor: 'black' }}>
+            <Image></Image>
+            <Text style ={{color: 'white', fontWeight: "bold"}}>ReadNews</Text>
+        </View>
+    )
+}
+
+export default ReadNews
