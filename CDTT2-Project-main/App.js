2ac41bb0bece85bf43aa8f9f1d65c77f7e86c3c2
Phuoc <phuoc0519@gmail.com>
2024-03-18 12:54:38 +0700

    commit moi

--- CDTT2-Project-main/App.js
+++ CDTT2-Project-main/App.js
@@ -1,9 +1,5 @@
-// Import React và các thành phần cần thiết từ React Native
 import React from 'react';
 
-// Import LoginScreen từ file bạn đã tạo
-
-// Component App chính sẽ render LoginScreen
 const App = () => {
   return (
   
