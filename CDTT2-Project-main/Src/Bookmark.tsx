0efd615f144f4e7393748af61f96490cac6eefd0
Phuoc <phuoc0519@gmail.com>
2024-03-22 15:55:23 +0700

    New file Bookmark

---
+++ CDTT2-Project-main/Src/Bookmark.tsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native";
+
+const Bookmark = () => {
+    
+    return (
+        <>
+        <Text>
+            HEloo
+        </Text>
+        </>
+    );
+}
+
+export default Bookmark;
